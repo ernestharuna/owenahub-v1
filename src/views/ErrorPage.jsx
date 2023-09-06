af3a3a4918e860b52f6dd1bd4f31317c0f619935
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2023-09-06 14:38:23 +0100

    Added a new button to Error page Added loading animation to error page

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,11 +1,13 @@
-import { useRouteError } from "react-router-dom"
+import { Link, useNavigation, useRouteError } from "react-router-dom"
 
 export default function ErrorPage() {
     const error = useRouteError();
+    const navigation = useNavigation();
+
     console.log(error);
 
     return (
-        <div id="error-page" style={style}>
+        <div id="error-page" style={style} className={navigation.state === "loading" ? " loading" : ""}>
             <h1>Oops</h1>
             <p>Sorry, an error occured.</p>
             <p className="text-secondary">
@@ -15,6 +17,12 @@ export default function ErrorPage() {
                 onClick={() => window.location.reload()}>
                 reload
             </button>
+            &nbsp;
+            <Link to={"/articles"}>
+                <button className="btn p-2">
+                    to articles
+                </button>
+            </Link>
         </div>
     )
 }
